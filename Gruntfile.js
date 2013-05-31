e65c328c983a2f3d8503e94bf13d4e5c0ab1a84f
Thomas.A <admin@stilva.com>
2013-05-31 14:29:20 +1000

    Added projects meta data

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      test : {
+        port : 8000
+      }
+    },
+
+    jasmine: {
+      taquet: {
+        src: ['src/**/*.js', '!src/vendor/require.js', '!src/vendor/r.js'],
+        options: {
+          specs: 'test/**/*Spec.js',
+          helpers: 'test/**/*Helper.js',
+          host: 'http://127.0.0.1:<%= connect.test.port %>/',
+          template: require('grunt-template-jasmine-requirejs'),
+          templateOptions: {
+            requireConfigFile: './src/main.js',
+            requireConfig: {
+              baseUrl: './src/'
+            }
+          }
+        }
+      }
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "./src/",
+          name: "main",
+          mainConfigFile: "./src/main.js",
+          out: "./js/main-built.js"
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+  // Default task(s).
+  grunt.registerTask('default', ['connect', 'jasmine']);
+
+  grunt.registerTask('compile', ['requirejs']);
+
+};
